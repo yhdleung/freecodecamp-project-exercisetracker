d003cee6aa2e5139699e518cb47a167f7e081ff2
Dick Leung <yhd.leung@gmail.com>
2022-05-09 17:10:50 +0800

    implement API endpoints for add and list users and exercises

--- README.md
+++ README.md
@@ -1,1 +1,93 @@
 # [Exercise Tracker](https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/exercise-tracker)
+
+
+## API endpoints
+
+### Create a New User  
+> POST /api/users  
+
+| Param    | sample value |
+| ---      | ---   |
+|username|yhdleung|
+
+```json
+{"username":"yhdleung","_id":"627630c21222d306bf0b36c2"}
+```
+
+### List all users
+> GET /api/users
+```json
+{"_id":"612109adf5860e05a3652f6b","username":"fcc_test_16295551170","__v":0},{"_id":"612109aef5860e05a3652f6d","username":"fcc_test_16295551175","__v":0},{"_id":"612109aff5860e05a3652f6f","username":"fcc_test_16295551182","__v":0},{"_id":"612109b0f5860e05a3652f71","username":"fcc_test_16295551193","__v":0},
+```
+
+### Add exercises
+> POST /api/users/:_id/exercises
+
+| Param    | sample value |
+| ---      | ---   |
+|:_id| 627630c21222d306bf0b36c2|
+|description| writing|
+|duration| 60|
+|date| 2022-05-07|
+
+```json
+{
+    "_id":"627630c21222d306bf0b36c2",
+    "username":"yhdleung",
+    "date":"Sat May 07 2022",
+    "duration":60,
+    "description":"writing"
+}
+```
+
+### GET user's exercise log 
+> GET /api/users/:_id/logs?[from][&to][&limit]  
+
+e.g.  api/users/627630c21222d306bf0b36c2/logs
+```json
+{
+    "_id": "627630c21222d306bf0b36c2",
+    "username": "yhdleung",
+    "count": 2,
+    "log": [
+        {
+            "description": "push up",
+            "duration": 60,
+            "date": "Sat May 07 2022"
+        },
+        {
+            "description": "writing",
+            "duration": 60,
+            "date": "Sat May 07 2022"
+        }
+    ]
+}
+```
+
+e.g.
+api/users/627630c21222d306bf0b36c2/logs?from=2022-03-10&to=2022-06-29
+| Param    | sample value |
+| ---      | ---   |
+|from| 2022-03-10|
+|to| 2022-06-29|
+```json
+{
+    "_id": "627630c21222d306bf0b36c2",
+    "username": "yhdleung",
+    "from": "Thu Mar 10 2022",
+    "to": "Fri Jun 10 2022",
+    "count": 2,
+    "log": [
+        {
+            "description": "push up",
+            "duration": 60,
+            "date": "Sat May 07 2022"
+        },
+        {
+            "description": "writing",
+            "duration": 60,
+            "date": "Sat May 07 2022"
+        }
+    ]
+}
+```
--- server.js
+++ server.js
@@ -1,15 +1,139 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
 require('dotenv').config()
 
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology:true});
+// console.log(mongoose.connection.readyState);
+const userSchema = new mongoose.Schema({ 
+  username: {type: String, required: true},
+});
+const exerciseSchema = new mongoose.Schema({ 
+  userId: {type: String, required: true},
+  description: {type: String, required: true},
+  duration: {type: Number, required: true},
+  date: Date
+});
+let User = mongoose.model('users', userSchema);
+let Exercise = mongoose.model('exercise', exerciseSchema);
+
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(cors())
 app.use(express.static('public'))
+
+// API endpoints
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+// Create a New User  
+app.post('/api/users', (req, res) => {
+  try {
+    let user = new User({username: req.body.username});
+    user.save((err, data) => {
+      if(err){
+        res.send("Error saving new user")
+      }
+      res.json(data)
+    })
+  } catch (error) {
+    console.log(error)
+    res.json({"error": error});
+  }
+})
 
+// List all users
+app.get('/api/users', (req, res) => {
+  User.find({}, (err, users)=>{
+    if(err || !users){
+      res.send("Error finding users")
+    }else{
+      res.json(users)
+    }
+  })
+})
+
+// Add exercise
+app.post('/api/users/:_id/exercises', (req, res) => {
+  try {
+    const userId = req.params._id
+    const {description, duration, date} = req.body
+    let exDate = date ? new Date(date) : new Date()
+
+    User.findById(userId, (err, userData) => {
+      if(err || !userData){
+        res.send("Error finding the user")
+      } else {
+        let exercise = new Exercise({
+          userId: userId,
+          description: description,
+          duration: Number(duration),
+          date: exDate
+        });
+        exercise.save((err, exData) => {
+          if(err || !exData){
+            res.send("Error saving new exercise")
+          } else {
+            res.json({
+              _id: userId,
+              username: userData.username,
+              description: description,
+              duration: Number(duration),
+              date: exDate.toDateString(),
+            })
+          }
+        })
+      }
+    })
+  } catch (error) {
+    console.log(error)
+    res.json({"error": error});
+  }
+})
+
+// get user's exercise log
+// GET /api/users/:_id/logs?[from][&to][&limit]  
+app.get('/api/users/:_id/logs', (req, res)=>{
+  const userId = req.params._id
+  const {from, to, limit} = req.query;
+  User.findById(userId, (err, userData) => {
+    if(err || !userData){
+      res.send("Error finding the user")
+    } else {
+      let filter = { userId }
+      if(from || to) {
+        let dateObj = {}
+        let dateRegex = /\d{4}-\d{2}-\d{2}/
+        if(from.match(dateRegex)){
+          dateObj["$gte"] = new Date(from)
+        }
+        if(to.match(dateRegex)){
+          dateObj["$lte"] = new Date(to)
+        }
+        filter.date = dateObj
+      }
+      Exercise.find(filter).limit(Number(limit)).exec((err, data)=>{
+        if(err || !data){
+          res.send("Cannot retrieve any exercise")
+        } else {
+          const log = data.map((d)=>({
+            description: d.description,
+            duration: d.duration,
+            date: d.date.toDateString()
+          }))
+          res.json({
+            username: userData.username,
+            count: data.length,
+            _id: userId,
+            log: log
+          })
+        }
+      })
+    }
+  }) 
+})
 
 
 
